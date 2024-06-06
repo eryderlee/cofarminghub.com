b76d4d65eacf747d8bcfa6c4711cd861140a13bb
Eden Lee <eryderlee@gmail.com>
2024-06-06 23:36:43 +1000

    bug fix3

--- netlify/functions/submit-contact-form.js
+++ netlify/functions/submit-contact-form.js
@@ -14,6 +14,7 @@ exports.handler = async (event) => {
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
         port: process.env.SMTP_PORT,
+        secure: false, // false for TLS (port 587), true for SSL (port 465)
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS,
